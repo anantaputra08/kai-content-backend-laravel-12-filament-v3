0d39d7ecb1fb3fc36b7f5ea7aca461c659141a79
PeYoT <putraananta08@gmail.com>
2025-06-06 12:00:00 +0700

    modify logic

--- database/seeders/CarriageSeeder.php
+++ database/seeders/CarriageSeeder.php
@@ -10,6 +10,14 @@ class CarriageSeeder extends Seeder
     public function run(): void
     {
         Carriages::insert([
+            ['name' => 'Gerbong 1'],
+            ['name' => 'Gerbong 2'],
+            ['name' => 'Gerbong 3'],
+            ['name' => 'Gerbong 4'],
+            ['name' => 'Gerbong 5'],
+            ['name' => 'Gerbong 6'],
+            ['name' => 'Gerbong 7'],
+            ['name' => 'Gerbong 8'],
             ['name' => 'Gerbong A'],
             ['name' => 'Gerbong B'],
             ['name' => 'Gerbong C'],
